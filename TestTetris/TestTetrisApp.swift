164a48ad529d4a203d7d3fc6c97e853af20c81ce
liuyang19900520 <liuyang19900520@hotmail.com>
2023-10-26 23:51:51 +0900

    Initial Commit

---
+++ TestTetris/TestTetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTetrisApp.swift
+//  TestTetris
+//
+//  Created by LiJiao on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TestTetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
